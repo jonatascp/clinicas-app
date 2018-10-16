bdef052c73ddba4cf68bb1882d9e15730ca26961
jonatascp <jonatascp@gmail.com>
2018-10-15 21:32:44 -0300

    create menu

---
+++ frontend/src/clinicas/clinicas.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Clinicas extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Clínicas</h1>
+            </div>
+        )
+    }
+}
